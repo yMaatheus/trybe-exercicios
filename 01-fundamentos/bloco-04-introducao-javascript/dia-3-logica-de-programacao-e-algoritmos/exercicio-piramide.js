07a45594a4123ca637c40916d5904a3b93d0419a
Matheus <stvgamer74@gmail.com>
2021-12-01 05:59:29 -0300

    Exercicio 4.3 - Piramide

---
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-3-logica-de-programacao-e-algoritmos/exercicio-piramide.js
@@ -0,0 +1,22 @@
+const n = 5;
+
+let array = [];
+
+for (let i = n; i >= 1; i -= 2) {
+    array.push(i);
+}
+
+array.sort(function(a, b){return a - b});
+
+for (let number of array) {
+    let spacesCount = Math.abs((n - number) / 2);
+    let spaces = "";
+    let piramideArea = "";
+    for (let i = 1; i <= spacesCount; i += 1) {
+        spaces += " ";
+    }
+    for (let i = 1; i <= number; i += 1) {
+        piramideArea += "*";
+    }
+    console.log(spaces + piramideArea);
+}
