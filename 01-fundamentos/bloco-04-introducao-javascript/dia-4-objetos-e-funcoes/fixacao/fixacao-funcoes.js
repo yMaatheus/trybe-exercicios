e2b2ca8ef2be7f4bd43bd15f8ec21ae62bea8b6f
Matheus <stvgamer74@gmail.com>
2021-12-05 14:57:37 -0300

    Exercicio 4.4 fixacao funcoes

---
+++ 01-fundamentos/bloco-04-introducao-javascript/dia-4-objetos-e-funcoes/fixacao/fixacao-funcoes.js
@@ -0,0 +1,19 @@
+function adicao(a, b) {
+  console.log(a + b);
+}
+
+function subtracao(a, b) {
+  console.log(a - b);
+}
+
+function multiplicacao(a, b) {
+  console.log(a * b);
+}
+
+function divisao(a, b) {
+  console.log(a / b);
+}
+
+function modulo(a, b) {
+  console.log(a % b);
+}
