205111a85e826a2244dc94364bfd435e68436053
Matheus <stvgamer74@gmail.com>
2022-01-08 16:57:43 -0300

    Exercicio 6.2 feito

--- 01-fundamentos/bloco-06-html-e-css-forms-flexbox-responsivo/dia-02-bibliotecas-javascript-e-framework-css/script.js
+++ 01-fundamentos/bloco-06-html-e-css-forms-flexbox-responsivo/dia-02-bibliotecas-javascript-e-framework-css/script.js
@@ -4,43 +4,58 @@ const states = ["AC - Acre", "AL - Alagoas", "AP - Amapá", "AM - Amazonas", "BA
     "RO - Rondônia", "RR - Roraíma", "SC - Santa Catarina", "SP - São Paulo", "SE - Sergipe", "TO - Tocantins"];
 const inputs = {
     name: {
-        input: document.querySelector("[name=name]"),
-        max: 40,
+        id: "#input-name",
+        maxLength: 40,
+        requireErrorMessage: "Informe seu Nome.",
     },
     email: {
-        input: document.querySelector("[name=email]"),
-        max: 50,
+        id: "#input-email",
+        maxLength: 50,
+        requireErrorMessage: "Informe seu Email.",
+        invalidErrorMessage: "Email inválido.",
     },
     cpf: {
-        input: document.querySelector("[name=cpf]"),
-        max: 11,
+        id: "#input-cpf",
+        maxLength: 11,
+        requireErrorMessage: "Informe seu CPF.",
+        numberErrorMessage: "Informe apenas numeros.",
     },
     address: {
-        input: document.querySelector("[name=address]"),
-        max: 200,
+        id: "#input-address",
+        maxLength: 200,
+        requireErrorMessage: "Informe seu endereço.",
     },
     city: {
-        input: document.querySelector("[name=city]"),
-        max: 28,
+        id: "#input-city",
+        maxLength: 28,
+        requireErrorMessage: "Informe sua cidade.",
+    },
+    state: {
+        id: "#input-state",
+        requireErrorMessage: "Estado não foi selecionado.",
     },
     house: {
-        input: document.querySelector("[name=house]"),
+        id: "#house-radio-group",
+        requireErrorMessage: "Selecione um tipo de imóvel."
     },
     curriculum: {
-        input: document.querySelector("[name=curriculum]"),
-        max: 1000,
+        id: "#textarea-curriculum",
+        maxLength: 1000,
+        requireErrorMessage: 'Informe um resumo do seu Curriculo.',
     },
     role: {
-        input: document.querySelector("[name=role]"),
-        max: 40,
+        id: "#input-role",
+        maxLength: 40,
+        requireErrorMessage: 'Informe seu cargo anterior.',
     },
-    description: {
-        input: document.querySelector("[name=desc]"),
-        max: 500,
+    desc: {
+        id: "#input-desc",
+        maxLength: 500,
+        requireErrorMessage: 'Informe uma descrição do seu cargo anterior.',
     },
-    startDate: {
-        input: document.querySelector("[name=date]"),
-        errorMessage: "",
+    date: {
+        id: "#input-date",
+        requireErrorMessage: 'Informe uma data.',
     },
 }
 
@@ -54,140 +69,10 @@ function loadStates() {
     }
 }
 
-function submit(event) {
-    event.preventDefault();
-
-    const div = document.querySelector("#exit-form");
-    let valid = true;
-
-    clearChilds(div);
-
-    for (const key in inputs) {
-        const obj = inputs[key];
-        if (key == "email") {
-            if (!validateEmail()) {
-                appendLine(div, key + " inválido.");
-                valid = false;
-                break;
-            }
-            continue;
-        }
-        if (key == "house") {
-            if (!validateHouse()) {
-                appendLine(div, key + " inválido.");
-                valid = false;
-                break;
-            }
-            continue;
-        }
-        if (key == "startDate") {
-            if (!validateStartDate()) {
-                appendLine(div, obj.errorMessage);
-                valid = false;
-                break;
-            }
-            continue;
-        }
-        if (!isValid(obj)) {
-            appendLine(div, key + " inválido.");
-            valid = false;
-            break;
-        }
-    }
-
-    if (valid) {
-        for (const key in inputs) {
-            const obj = inputs[key];
-            appendLine(div, key + ": " + obj.input.value);
-        }
-    }
-}
-
-function isValid(obj) {
-    const input = obj.input;
-    const max = obj.max;
-    const value = input.value;
-    const length = value.length;
-    if (isNull(input) || !isValidLength(length, max)) {
-        return false;
-    }
-    return true;
-}
-
-function validateEmail() {
-    const obj = inputs.email;
-    const max = obj.max;
-    const input = obj.input;
-    const value = input.value;
-    const length = input.length;
-    if (isNull(input) || !isValidLength(length, max) || !value.includes("@")) {
-        return false;
-    }
-    return true;
-}
-
-function validateHouse() {
-    const input = inputs.house.input;
-    if (isNull(input) || input.value === "") {
-        return false;
-    }
-    return true;
-}
-
-function validateStartDate() {
-    const date = inputs.startDate;
-    const input = date.input;
-    const value = input.value;
-    const length = value.length;
-    if (isNull(input) || !isValidLength(length)) {
-        date.errorMessage = "Data de início não foi preenchida.";
-        return false;
-    }
-    const df = value.split("/");
-    const day = df[0];
-    const month = df[1];
-    const year = df[2];
-
-    if (!(df.length === 3 && day.length == 2 && month.length === 2 && year.length === 4)) {
-        date.errorMessage = "Formato da data de início é inválida.";
-        return false;
-    }
-
-    if (!(day > 0 && day <= 31)) {
-        date.errorMessage = "O dia da data de inicio é inválido.";
-        return false;
-    }
-
-    if (!(month > 0 && month <= 12)) {
-        date.errorMessage = "O mês da data de inicio é inválido.";
-        return false;
-    }
-
-    if (year < 0) {
-        date.errorMessage = "O ano da data de inicio é inválido.";
-        return false;
-    }
-    return true;
-}
-
 function appendLine(father, str) {
     const p = document.createElement("p");
     p.innerHTML = str;
     father.appendChild(p);
-}
-
-function isNull(arg) {
-    if (arg === null || arg.value === null) {
-        return true;
-    }
-    return false;
-}
-
-function isValidLength(length, max) {
-    if (length <= 0 || length > max) {
-        return false;
-    }
-    return true;
 }
 
 function clearChilds(arg) {
@@ -204,30 +89,13 @@ function clear() {
     form.reset();
 }
 
-function formatDate(event) {
-    const target = event.target;
-    const key = event.key;
-    const value = target.value;
-    const length = value.length;
-    if (isNaN(key) || event.key === " ") {
-        event.preventDefault();
-    }
-    if (length === 2 || length === 5) {
-        target.value = value.concat("/");
-    }
-}
-
 window.onload = function () {
     loadStates();
-
-    const submitButton = document.querySelector(".button-submit");
-    submitButton.addEventListener('click', submit);
 
     const clearButton = document.querySelector(".button-clear");
     clearButton.addEventListener('click', clear)
 
     const inputDate = document.querySelector("#input-date");
-    // inputDate.addEventListener("keypress", formatDate);
     inputDate.DatePickerX.init({
         format: "dd/mm/yyyy",
         weekDayLabels: ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom'],
@@ -238,5 +106,75 @@ window.onload = function () {
         clearButtonLabel: "Limpar",
     });
 
-    const validate = new window.JustValidate('#form');
+    const validate = new JustValidate('#form');
+
+    validate.addField(inputs.name.id, [{
+        rule: 'required',
+        errorMessage: inputs.name.requireErrorMessage,
+    }, {
+        rule: 'maxLength',
+        value: inputs.name.maxLength,
+    }]).addField(inputs.email.id, [{
+        rule: 'required',
+        errorMessage: inputs.email.requireErrorMessage,
+    }, {
+        rule: 'email',
+        errorMessage: inputs.email.invalidErrorMessage,
+    }, {
+        rule: 'maxLength',
+        value: inputs.email.maxLength,
+    }]).addField(inputs.cpf.id, [{
+        rule: 'required',
+        errorMessage: inputs.cpf.requireErrorMessage,
+    }, {
+        rule: 'maxLength',
+        value: inputs.cpf.maxLength,
+    }, {
+        rule: 'number',
+        errorMessage: inputs.cpf.numberErrorMessage,
+    }]).addField(inputs.address.id, [{
+        rule: 'required',
+        errorMessage: inputs.address.requireErrorMessage,
+    }, {
+        rule: 'maxLength',
+        value: inputs.address.maxLength,
+    }]).addField(inputs.city.id, [{
+        rule: 'required',
+        errorMessage: inputs.city.requireErrorMessage,
+    }, {
+        rule: 'maxLength',
+        value: inputs.city.maxLength,
+    }]).addField(inputs.state.id, [{
+        rule: 'required',
+        errorMessage: inputs.state.requireErrorMessage,
+    }]).addRequiredGroup(inputs.house.id, inputs.house.requireErrorMessage).addField(inputs.curriculum.id, [{
+        rule: 'required',
+        errorMessage: inputs.curriculum.requireErrorMessage,
+    }, {
+        rule: 'maxLength',
+        value: inputs.curriculum.maxLength,
+    }]).addField(inputs.role.id, [{
+        rule: 'required',
+        errorMessage: inputs.role.requireErrorMessage,
+    }, {
+        rule: 'maxLength',
+        value: inputs.role.maxLength,
+    }]).addField(inputs.desc.id, [{
+        rule: 'required',
+        errorMessage: inputs.desc.requireErrorMessage,
+    }, {
+        rule: 'maxLength',
+        value: inputs.desc.maxLength,
+    }]).addField(inputs.date.id, [{
+        rule: 'required',
+        errorMessage: inputs.date.requireErrorMessage,
+    }]).onSuccess((event) => {
+        // console.log('Validação aprovada e enviada.', event);
+        const div = document.querySelector("#exit-form");
+        clearChilds(div);
+
+        for (const key in inputs) {
+            appendLine(div, key + ": " + document.querySelector("[name=" + key + "]").value);
+        }
+    });
 }
