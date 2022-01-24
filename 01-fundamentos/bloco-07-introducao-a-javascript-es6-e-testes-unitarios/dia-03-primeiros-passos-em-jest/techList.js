07a6ca3453b90d6fcd6172489a0ab51db324e162
Matheus <stvgamer74@gmail.com>
2022-01-24 20:28:48 -0300

    Exercicio 7.3 - funcao techList refatorada e aprovada nos testes!

---
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/techList.js
@@ -0,0 +1,10 @@
+function techList(array, name) {
+    if (array.length <= 0) {
+        return 'Vazio!';
+    }
+    array.sort();
+    return array.map((tech) => ({ tech, name }));
+}
+
+console.log(techList(["React", "Jest", "HTML", "CSS", "JavaScript"], 'Lucas'));
+module.exports = techList;
