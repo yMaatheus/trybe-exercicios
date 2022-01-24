7e3ec76f42a0d4fdd3b43dac74335bb280c645b7
Matheus <stvgamer74@gmail.com>
2022-01-24 19:41:29 -0300

    Exercicio 7.3 - Exercicio MyFizzBuzz feito!

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/myFizzBuzz.test.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/myFizzBuzz.test.js
@@ -2,12 +2,18 @@ const myFizzBuzz = require('./myFizzBuzz');
 
 describe('Exercicio 3', () => {
     it('Chame a função com um numero dividido por 3 e 5 e espere o retorno "fizzbuzz"', () => {
-
+        expect(myFizzBuzz(30)).toBe('fizzbuzz');
     });
-    it('', () => {
-
+    it('Chame a função com um número divisível por 3 e verifique se o retorno "fizz"', () => {
+        expect(myFizzBuzz(18)).toBe('fizz');
     });
-    it('', () => {
-
+    it('Chame a função com um número divisível por 5 e verifique se o retorno "buzz"', () => {
+        expect(myFizzBuzz(20)).toBe('buzz');
+    });
+    it('Chame a função com um número que não é divisível por 3 ou 5 e verifique se o retorno é o próprio número', () => {
+        expect(myFizzBuzz(11)).toBe(11);
+    });
+    it('Chame a função com um parâmetro que não é um número e verifique se o retorno é false', () => {
+        expect(myFizzBuzz('')).toBe(false);
     });
 });
