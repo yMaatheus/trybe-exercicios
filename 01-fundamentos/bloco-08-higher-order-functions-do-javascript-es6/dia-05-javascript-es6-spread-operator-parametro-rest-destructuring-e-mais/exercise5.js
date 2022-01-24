a76e3123a6d5a12dfed7d6a0fa1bbf4ddd0dbd6d
Matheus <stvgamer74@gmail.com>
2022-01-24 18:51:01 -0300

    Exercicio 8.5 - Exercicio 5 feito!

---
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise5.js
@@ -0,0 +1,7 @@
+const myList = [1, 2, 3];
+
+// const swap = (array) => array = [array[2], array[1], array[0]];
+
+const swap = ([a, b, c]) => [c, b, a]; // Jeito simplificado!
+
+console.log(swap(myList));
