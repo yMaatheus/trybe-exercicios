b59fce7920fa75823ef3883df59401425b445243
Matheus <stvgamer74@gmail.com>
2022-01-24 21:28:04 -0300

    Passando todos os testes para a pasta de testes

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/tests/myFizzBuzz.test.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/tests/myFizzBuzz.test.js
@@ -1,4 +1,4 @@
-const myFizzBuzz = require('./myFizzBuzz');
+const myFizzBuzz = require('../myFizzBuzz');
 
 describe('Exercicio 3', () => {
     it('Chame a função com um numero dividido por 3 e 5 e espere o retorno "fizzbuzz"', () => {
