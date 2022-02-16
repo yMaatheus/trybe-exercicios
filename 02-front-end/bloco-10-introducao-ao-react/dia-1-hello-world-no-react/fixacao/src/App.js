095d86a31a0a95cdb88b8e1e1c9be6f475f4e4dd
Matheus <stvgamer74@gmail.com>
2022-02-16 18:39:24 -0300

    Exercicio 10.1 - Fixacao

---
+++ 02-front-end/bloco-10-introducao-ao-react/dia-1-hello-world-no-react/fixacao/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import AboutMe from './Component';
+
+function App() {
+  return (
+    <AboutMe/>
+  );
+}
+
+export default App;
---
+++ 02-front-end/bloco-10-introducao-ao-react/dia-1-hello-world-no-react/fixacao/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class AboutMe extends Component {
+    render() {
+        return (
+            <>
+            <h1>Matheus Henrique</h1>
+            <p>Olá me chamo Matheus, tenho 18 anos e sou uma pessoa estudante na Trybe</p>
+            </>
+        );
+    }
+}
+
+export default AboutMe;
