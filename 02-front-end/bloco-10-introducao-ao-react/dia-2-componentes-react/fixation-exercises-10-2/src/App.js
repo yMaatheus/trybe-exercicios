ea89119b20bea1f537c7b0f808958d15766250f0
Matheus <stvgamer74@gmail.com>
2022-02-17 15:24:56 -0300

    Exercicio 10.2 - Fixacao adicionando componente Image com props

--- 02-front-end/bloco-10-introducao-ao-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
+++ 02-front-end/bloco-10-introducao-ao-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
@@ -1,25 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Image from './components/Image'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return (<Image alternativeText='Cute cat staring' />);
+  return (<Image source={logo} alternativeText='Cute cat staring' />);
 }
 
 export default App;
