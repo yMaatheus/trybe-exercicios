7412f184e470a810e9c5116cde43eb03eb9887c4
Matheus <stvgamer74@gmail.com>
2022-02-17 15:54:55 -0300

    Exercicio 10.2 - Fixacao usando a props com o Order.jsx

--- 02-front-end/bloco-10-introducao-ao-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
+++ 02-front-end/bloco-10-introducao-ao-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
@@ -1,10 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import Image from './components/Image'
+import Order from './components/Order';
 
-function App() {
-  // return (<Image alternativeText='Cute cat staring' />);
-  return (<Image source={logo} alternativeText='Cute cat staring' />);
+class App extends React.Component {
+  render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
+
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
+    return (
+      <div className="App">
+        <h1> Orders recently created </h1>
+         <Order order={headphone}/>
+         <Order order={energyDrink}/>
+      </div>
+    );
+  }
 }
 
 export default App;
