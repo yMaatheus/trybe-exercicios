106e3dcee15db884671290a06befa48b384f31ad
Matheus <stvgamer74@gmail.com>
2022-02-25 18:28:21 -0300

    Exercicio 11.1 - Fixaçao Fancy Buttons App finalizado!

--- 02-front-end/bloco-11-componentes-com-estado-eventos-e-formulários-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/components/Button.jsx
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-e-formulários-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/components/Button.jsx
@@ -7,19 +7,22 @@ class Button extends React.Component {
             count: 0
         }
         this.count = this.count.bind(this);
+        this.getButtonColor = this.getButtonColor.bind(this);
     }
 
     count() {
-        this.setState((currentState, _props) => ({
+        this.setState((currentState) => ({
             count: currentState.count + 1
-        }));
+        }), () => console.log(`${this.props.name} ${this.getButtonColor()}`));
     }
+
+    getButtonColor = () => (this.state.count % 2 === 0) ? 'green' : 'white';
 
     render() {
         const { name, message } = this.props;
         return (
         <>
-            <button onClick={() => {this.count();message()}}>{`${name} - Cliques: ${this.state.count}`}</button>
+            <button onClick={() => {this.count();message()}} style={{backgroundColor: this.getButtonColor()}} >{`${name} - Cliques: ${this.state.count}`}</button>
         </>);
     }
 }
