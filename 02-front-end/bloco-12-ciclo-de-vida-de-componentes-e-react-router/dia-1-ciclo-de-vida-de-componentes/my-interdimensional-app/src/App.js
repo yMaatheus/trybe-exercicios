2651dca8909b54673c851ce2f3b9cb50fec4d2d2
Matheus <stvgamer74@gmail.com>
2022-03-10 18:55:00 -0300

    Exercicio 12.1 - Fixacao - Utilizando componentDidMount para renderizar itens na tela apos requisiçao

--- 02-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/my-interdimensional-app/src/App.js
+++ 02-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/my-interdimensional-app/src/App.js
@@ -18,12 +18,27 @@ class App extends Component {
     })
   }
 
+  componentDidMount() {
+    this.fetchCharacters();
+  }
+
   render() {
+    const { characters } = this.state;
     return (
       <div className="App">
         <h1>
           Ricky and Morty Characters:
         </h1>
+        <div className="body">
+          {characters.map(({ name, image }) => {
+            return (
+              <div className="container" key={name}>
+                <h3>{name}</h3>
+                <img src={image} alt={name}/>
+              </div>
+            )
+          })}
+        </div>
       </div>
     );
   }
