4fc4d0b490c5bcf830d084fd8e4f34d023649d9b
Matheus <stvgamer74@gmail.com>
2022-03-11 15:52:01 -0300

    Exercicio 12.1 - Fixacao - Criando constructor, componentDidMount, requisiçao fetch, shouldComponentUpdate e renderizaçao condicional

--- 02-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/randomuser-app/src/components/RandomUser.jsx
+++ 02-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/randomuser-app/src/components/RandomUser.jsx
@@ -1,10 +1,56 @@
 import React from "react";
 
 class RandomUser extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            isLoading: true,
+            data: undefined,
+        }
+        this.fetchRandomUser = this.fetchRandomUser.bind(this);
+    }
+
+    componentDidMount() {
+        this.fetchRandomUser();
+    }
+
+    async fetchRandomUser() {
+        this.setState({
+            isLoading: true
+        }, async () => {
+            const response = await fetch('https://api.randomuser.me/');
+            const { results } = await response.json();
+            this.setState({
+                isLoading: false,
+                data: results[0],
+            });
+        })
+    }
+
+    shouldComponentUpdate(_nextProps, { data }) {
+        // Caso a idade do usuário seja maior que 50, o componente NÃO deve ser renderizado. Aplique esta lógica no método shouldComponentUpdate.
+        console.log(`shouldComponentUpdate: ${(data) ? data.dob.age < 50 : true}`);
+        return (data) ? data.dob.age < 50 : true;
+    }
 
     render() {
+        const { isLoading, data: { name: { title, first, last } = '', email, dob: { age } = '', picture: { medium: avatar } = '' } = '' } = this.state;
+        const loading = <div>loading...</div>
+        const card = (
+            <section>
+                <h1>{`${title}. ${first} ${last}`}</h1>
+                <span>{email}</span>
+                <br></br>
+                <span>{age}</span>
+                <div>
+                    <img src={avatar} alt="Avatar" />
+                </div>
+            </section>
+        );
         return (
-            <></>
+            <>
+                {isLoading ? loading : card}
+            </>
         );
     }
 }
