24db60b1a06bcb225f5054150556d060cc759d79
Matheus <stvgamer74@gmail.com>
2022-03-11 04:47:02 -0300

    Exercicio 12.1 - Fixacao - Criando react-app e Component RandomUser

---
+++ 02-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/randomuser-app/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import RandomUser from './components/RandomUser';
+
+function App() {
+  return (
+    <RandomUser />
+  );
+}
+
+export default App;
---
+++ 02-front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/randomuser-app/src/components/RandomUser.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class RandomUser extends React.Component {
+
+    render() {
+        return (
+            <></>
+        );
+    }
+}
+
+export default RandomUser;
