9d30920416efec0eaada4e3b18c6427577496228
Matheus <stvgamer74@gmail.com>
2022-03-19 20:18:23 -0300

    Exercicio 14.2 - Parte 1 concluida, testando as funçoes: randomNumber, toUpper, firstChar, concatStrings, fetchDogPicture

--- 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicio/service.js
+++ 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicio/service.js
@@ -1,9 +1,27 @@
-function numeroAleatorio() {
+const fetch = require('node-fetch');
+
+function randomNumber() {
     return Math.random() * 100;
 }
 
-function a() {
-
+function toUpper(str) {
+    return str.toUpperCase();
 }
 
-module.exports = { numeroAleatorio, a};
+function firstChar(str) {
+    return str.charAt(0);
+}
+
+function concatStrings(str1, str2) {
+    return str1.concat(str2);
+}
+
+async function fetchDogPicture() {
+    const url = 'https://dog.ceo/api/breeds/image/random';
+
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+module.exports = { randomNumber, toUpper, firstChar, concatStrings, fetchDogPicture };
--- 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicio/service.test.js
+++ 02-front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicio/service.test.js
@@ -1,9 +1,86 @@
-let { numeroAleatorio, a } = require('./service.js');
+let service = require('./service.js');
 
-describe('Testando função "numeroAleatorio".', () => {
+describe('Testando função "randomNumber".', () => {
     it('A função foi chamada.', () => {
-        numeroAleatorio = jest.fn();
-        numeroAleatorio();
-        expect(numeroAleatorio).toHaveBeenCalled();
+        service.randomNumber = jest.fn();
+
+        service.randomNumber();
+        expect(service.randomNumber).toHaveBeenCalled();
     });
+
+    it('A função retorna o esperado.', () => {
+        service.randomNumber = jest.fn().mockReturnValue(10);
+
+        expect(service.randomNumber()).toBe(10);
+    });
+
+    it('A função foi teve suas execuções chamadas corretamente.', () => {
+        service.randomNumber = jest.fn();
+
+        service.randomNumber();
+        service.randomNumber();
+        service.randomNumber();
+        expect(service.randomNumber).toHaveBeenCalledTimes(3);
+    });
+
+    it('A função foi chamada e a nova implementação de divisão foi aplicada e acontece apenas uma vez.', () => {
+        service.randomNumber = jest.fn().mockImplementation(() => 1).mockImplementationOnce((a, b) => a/b);
+
+        expect(service.randomNumber(4, 2)).toBe(2);
+        expect(service.randomNumber()).toBe(1);
+    });
+
+    it('Desenvolva uma nova implementação que receba três parâmetros multiplicando-os e resete sua implementação e crie uma nova, que receba apenas um parâmetro e retorne o dobro', () => {
+        service.randomNumber = jest.fn().mockImplementation((a, b, c) => a*b*c);
+
+        expect(service.randomNumber(2, 2, 2)).toBe(8);
+
+        service.randomNumber.mockReset();
+        service.randomNumber = jest.fn().mockImplementation((a) => a *2);
+
+        expect(service.randomNumber(10)).toBe(20);
+    });
+});
+
+describe('Testando as funções: "toUpper", "firstChar" e "concatStrings"', () => {
+    it('Desenvolva uma nova implementação para a primeira função: agora ela deve retornar a string em caixa baixa.', () => {
+        service.toUpper = jest.spyOn(service, "toUpper").mockImplementation((str) => str.toLowerCase());
+
+        expect(service.toUpper('BOM DIA')).toBe('bom dia');
+
+        service.toUpper.mockRestore();
+        expect(service.toUpper('abc')).toBe('ABC');
+    });
+
+    it('Desenvolva para a segunda função, uma nova implementação: ela deve retornar a última letra de uma string.', () => {
+        service.firstChar = jest.fn().mockImplementation((str) => str.charAt(str.length -1));
+
+        expect(service.firstChar('BOM DIA')).toBe('A');
+    });
+
+    it('Desenvolva para a terceira função: ela deve receber três strings e concatená-las.', () => {
+        service.concatStrings = jest.fn().mockImplementation((a, b, c) => a.concat(b).concat(c));
+
+        expect(service.concatStrings('BOM DIA', ' Pessoa ', 'Estudante')).toBe('BOM DIA Pessoa Estudante');
+    });
+})
+
+describe('Testando a função: "fetchDogPicture"', () => {
+    test('A requisição se resolveu e teve como valor "request sucess"', async () => {
+        service.fetchDogPicture = jest.fn().mockResolvedValue("request sucess");
+    
+        service.fetchDogPicture();
+        expect(service.fetchDogPicture).toHaveBeenCalled();
+        expect(service.fetchDogPicture).toHaveBeenCalledTimes(1);
+        await expect(service.fetchDogPicture()).resolves.toBe("request sucess");
+        expect(service.fetchDogPicture).toHaveBeenCalledTimes(2);
+      });
+    
+      test('A requisição falhou e ter como valor "request failed"', async () => {
+        service.fetchDogPicture = jest.fn().mockRejectedValue("request failed");
+    
+        expect(service.fetchDogPicture).toHaveBeenCalledTimes(0);
+        await expect(service.fetchDogPicture()).rejects.toMatch("request failed");
+        expect(service.fetchDogPicture).toHaveBeenCalledTimes(1);
+      });
 });
