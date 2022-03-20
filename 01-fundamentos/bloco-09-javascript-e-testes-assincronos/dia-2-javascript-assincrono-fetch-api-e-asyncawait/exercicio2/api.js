31d8d13c6823a687692c53af677361b4fe1d5752
Matheus <stvgamer74@gmail.com>
2022-03-19 21:26:13 -0300

    Exercicio 9.2 - Exercicio 2 feito

--- 01-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/exercicio2/api.js
+++ 01-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/exercicio2/api.js
@@ -1,6 +1,24 @@
+const list = document.querySelector('#coins-list');
+
 const fetchCoins = async () => {
-    const coins = await fetch('https://api.coincap.io/v2/assets').then(response => response.json()).then(data => console.log(data));
-    return coins;
+    const resolve = await fetch('https://api.coincap.io/v2/assets');
+    const { data } = await resolve.json();
+    return data;
 };
 
-window.onload = () => fetchCoins();
+const fetchPriceBrl = async () => {
+    const resolve = await fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json');
+    const { usd: { brl } } = await resolve.json();
+    return brl;
+}
+
+window.onload = async () => {
+    const array = await fetchCoins();
+    const priceBrl = await fetchPriceBrl();
+    array.filter(({rank}) => (rank <= 10)).forEach(({name, symbol, priceUsd}) => {
+        const li = document.createElement('li');
+        const price = Number(priceUsd * priceBrl).toFixed(2);
+        li.innerText = `${name} (${symbol}): ${price}`;
+        list.appendChild(li);
+    });
+};
