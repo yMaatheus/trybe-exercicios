2e4f837854f96b88c5d8ef7cb1fd697a9412b456
yMaatheus <stvgamer74@gmail.com>
2022-05-12 16:00:37 -0300

    feat: Executando container com terminal apartir da imagem criada e vendo informações sobre o sistema operacional.

--- 03-back-end/bloco-19-docker/dia-1-utilizando-containers-docker/exercise.cmd
+++ 03-back-end/bloco-19-docker/dia-1-utilizando-containers-docker/exercise.cmd
@@ -1,3 +1,15 @@
 ## Baixando imagem do Debian versão reduzida:
 
 docker pull debian:stable-slim
+
+## Criando container a partir da imagem:
+
+docker container run -it debian:stable-slim
+
+## Vendo os dados do sistema operacional do container:
+
+cat /etc/*-release
+
+## Encerrando o terminal do container:
+
+exit
