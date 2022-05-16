8113b80e55942867f75559ae7bdd2136bad72c23
yMaatheus <stvgamer74@gmail.com>
2022-05-16 11:26:26 -0300

    feat: executando container com mensagem personalizada

--- 03-back-end/bloco-19-docker/dia-2-manipulacao-e-criacao-de-imagens-no-docker/exercise.cmd
+++ 03-back-end/bloco-19-docker/dia-2-manipulacao-e-criacao-de-imagens-no-docker/exercise.cmd
@@ -2,6 +2,10 @@
 
 docker image build -t cowsay .
 
-# Criando um container com a imagem padrão:
+# Criando um container com comando padrão padrão:
 
 docker container run cowsay
+
+# Criando um container com comando personalizado:
+
+docker container run cowsay hello
