4f8797ea851182eb48bd2771ea26fe10ac517eb1
yMaatheus <stvgamer74@gmail.com>
2022-05-20 07:10:57 -0300

    refactor: atualizando caminho dos arquivos feitos no exercicio 1 para a pasta exercise-1

--- 03-back-end/bloco-19-docker/dia-3-orquestrando-containers-com-docker-compose/exercise-1/exercise.cmd
+++ 03-back-end/bloco-19-docker/dia-3-orquestrando-containers-com-docker-compose/exercise-1/exercise.cmd
@@ -1,6 +1,6 @@
 ## Criando container com servidor apache com porta 4545 utilizando volume para obter a source code:
 
-docker container run -d -p 4545:80 -v "/home/matheus/workspace/exercises/trybe-exercicios/03-back-end/bloco-19-docker/dia-3-orquestrando-containers-com-docker-compose:/usr/local/apache2/htdocs/" httpd:2.4
+docker container run -d -p 4545:80 -v "/home/matheus/workspace/exercises/trybe-exercicios/03-back-end/bloco-19-docker/dia-3-orquestrando-containers-com-docker-compose/exercise-1/:/usr/local/apache2/htdocs/" httpd:2.4
 
 ## Obtendo o id do container:
 
@@ -20,7 +20,7 @@ docker rm 80db1dde6def
 
 ## Verificando se o arquivo html está no mesmo lugar:
 
-cd /home/matheus/workspace/exercises/trybe-exercicios/03-back-end/bloco-19-docker/dia-3-orquestrando-containers-com-docker-compose
+cd /home/matheus/workspace/exercises/trybe-exercicios/03-back-end/bloco-19-docker/dia-3-orquestrando-containers-com-docker-compose/exercise-1/
 ls -la
 
 ## Obtando o id da imagem:
