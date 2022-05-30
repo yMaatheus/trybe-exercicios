b4ff2d5e3671f6c583863d0a9d4308d35999edf6
yMaatheus <stvgamer74@gmail.com>
2022-05-30 17:12:28 -0300

    feat(Bonus): Exibindo todas as informações das cinco peças com os maiores preços

--- 03-back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/bonus/exercise-bonus.cmd
+++ 03-back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/bonus/exercise-bonus.cmd
@@ -1,3 +1,7 @@
 ## Escreva uma query para exibir a peça e o preço de tudo que é provido pela empresa RBT:
 
 SELECT Piece, Price FROM PiecesProviders.Provides WHERE Provider = 'RBT';
+
+## Escreve uma query para exibir todas as informações das cinco peças com os maiores preços:
+
+SELECT * FROM PiecesProviders.Provides ORDER BY Price DESC LIMIT 5;
