bf4c9689d032afd862d5aa84f3705042d04819eb
yMaatheus <stvgamer74@gmail.com>
2022-05-30 18:15:35 -0300

    feat: exibindo todos os ids de cientistas cujos projetos sejam AeH3, Ast3 ou Che1

--- 03-back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/bonus/bonus-exercise.sql
+++ 03-back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/bonus/bonus-exercise.sql
@@ -6,3 +6,6 @@ SELECT Name FROM Scientists.Projects WHERE Name LIKE '%A%' ORDER BY Name;
 
 -- exibindo o código e nome de todos os projetos que possuam em seu código o número 3 em ordem alfabética:
 SELECT Code, Name FROM Scientists.Projects WHERE Code LIKE '%3%' ORDER BY Name;
+
+-- exibindo todos os ids de cientistas cujos projetos sejam AeH3, Ast3 ou Che1:
+SELECT Scientist FROM Scientists.AssignedTo WHERE Project IN ('AeH3', 'Ast3','Che1');
