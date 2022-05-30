181e4af71798bc30a2e72f5f6dba2e7db8751165
yMaatheus <stvgamer74@gmail.com>
2022-05-30 20:43:02 -0300

    feat: exibindo a média salarial de cada JOB_ID, ordenando pela média salarial em ordem decrescente

--- 03-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercise.sql
+++ 03-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercise.sql
@@ -3,3 +3,6 @@ SELECT MAX(SALARY) FROM hr.employees;
 
 -- exibindo a diferença entre o maior e o menor salário:
 SELECT (MAX(SALARY) - MIN(SALARY)) FROM hr.employees;
+
+-- exibindo a média salarial de cada JOB_ID, ordenando pela média salarial em ordem decrescente:
+SELECT JOB_ID, AVG(SALARY) AS avg_job FROM hr.employees GROUP BY JOB_ID ORDER BY avg_job DESC
