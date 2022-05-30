c9c4abe42a2188c15a4f76ff1dcb60394ac70b04
yMaatheus <stvgamer74@gmail.com>
2022-05-30 17:47:58 -0300

    feat: exibindo as peças, preço e fornecedor de todos os fornecimentos em que a sigla do fornecedor tenha a letra N

--- 03-back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise.sql
+++ 03-back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise.sql
@@ -1,6 +1,10 @@
--- Exibindo todas as peças que começam com as letras GR:
+-- exibindo todas as peças que começam com as letras GR:
 SELECT * FROM PecasFornecedores.Pecas WHERE name LIKE 'GR%';
 
 -- escreva uma query para mostrar todos os fornecimentos que contenham a peça com code 2 em ordem alfabética de fornecedor:
 
 SELECT * FROM PecasFornecedores.Fornecimentos WHERE code = 2 ORDER BY Fornecedor;
+
+-- exibindo as peças, preço e fornecedor de todos os fornecimentos em que a sigla do fornecedor tenha a letra N:
+
+SELECT peca, Preco, Fornecedor FROM PecasFornecedores.Fornecimentos WHERE Fornecedor LIKE '%N%';
