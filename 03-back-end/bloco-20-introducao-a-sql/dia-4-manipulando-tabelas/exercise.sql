3c4f2155f8a53495b1a033fc85832984dec8676a
yMaatheus <stvgamer74@gmail.com>
2022-05-30 18:40:02 -0300

    feat: corrigindo o nome do diretor do filme Procurando Nemo para Andrew Staton

--- 03-back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercise.sql
+++ 03-back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercise.sql
@@ -5,3 +5,7 @@ VALUES ('Monstros SA', 'Pete Docter', 2001, 92), ('Procurando Nemo', 'John Lasse
 
 -- inserindo na tabela BoxOffice que o filme Procurando Nemo foi classificado em 6.8, fez 450 milhões no mercado interno e 370 milhões no mercado internacional:
 INSERT INTO Pixar.BoxOffice (movie_id, rating, domestic_sales, international_sales) VALUES (9, 6.8, 450000000, 370000000);
+
+-- corrigindo o nome do diretor do filme Procurando Nemo para Andrew Staton:
+SET SQL_SAFE_UPDATES = 0;
+UPDATE Pixar.Movies SET director = 'Andrew Staton' WHERE title = 'Procurando Nemo';
