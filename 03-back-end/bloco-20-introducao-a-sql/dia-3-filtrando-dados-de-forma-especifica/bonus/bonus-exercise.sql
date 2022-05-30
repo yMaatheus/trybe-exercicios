f818840ffb577b4df033ecbba7299fece66623c3
yMaatheus <stvgamer74@gmail.com>
2022-05-30 18:10:53 -0300

    feat: exibindo o nome de todos os projetos cujo o código inicie com a letra A em ordem alfabética

--- 03-back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/bonus/bonus-exercise.sql
+++ 03-back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/bonus/bonus-exercise.sql
@@ -1,2 +1,5 @@
 -- exibindo todas as informações de todos os cientistas que possuam a letra e em seu nome:
 SELECT * FROM Scientists.Scientists WHERE Name LIKE '%e%';
+
+-- exibindo o nome de todos os projetos cujo o código inicie com a letra A em ordem alfabética:
+SELECT Name FROM Scientists.Projects WHERE Name LIKE '%A%' ORDER BY Name;
