0004c06bddd804d8ac6ae38b317ef70289f050a3
yMaatheus <stvgamer74@gmail.com>
2022-05-30 18:12:46 -0300

    feat: exibindo o código e nome de todos os projetos que possuam em seu código o número 3 em ordem alfabética

--- 03-back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/bonus/bonus-exercise.sql
+++ 03-back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/bonus/bonus-exercise.sql
@@ -3,3 +3,6 @@ SELECT * FROM Scientists.Scientists WHERE Name LIKE '%e%';
 
 -- exibindo o nome de todos os projetos cujo o código inicie com a letra A em ordem alfabética:
 SELECT Name FROM Scientists.Projects WHERE Name LIKE '%A%' ORDER BY Name;
+
+-- exibindo o código e nome de todos os projetos que possuam em seu código o número 3 em ordem alfabética:
+SELECT Code, Name FROM Scientists.Projects WHERE Code LIKE '%3%' ORDER BY Name;
