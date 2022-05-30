38c5aea01cc8712f3f058efd6b6fc4ed946012ca
yMaatheus <stvgamer74@gmail.com>
2022-05-30 17:14:47 -0300

    feat(bonus): Exibindo o nome das empresas e preço das peças com os quatro maiores preços, começando a lista a partir do 3º item.

--- 03-back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/bonus/exercise-bonus.cmd
+++ 03-back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/bonus/exercise-bonus.cmd
@@ -5,3 +5,7 @@ SELECT Piece, Price FROM PiecesProviders.Provides WHERE Provider = 'RBT';
 ## Escreve uma query para exibir todas as informações das cinco peças com os maiores preços:
 
 SELECT * FROM PiecesProviders.Provides ORDER BY Price DESC LIMIT 5;
+
+## Escreva uma query para exibir o nome das empresas e preço das peças com os quatro maiores preços, começando a lista a partir do 3º item.
+
+SELECT Provider, Price FROM PiecesProviders.Provides ORDER BY Price DESC LIMIT 4 OFFSET 2;
