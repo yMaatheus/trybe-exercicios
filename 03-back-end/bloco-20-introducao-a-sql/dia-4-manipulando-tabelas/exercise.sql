f1a10ebb75c83c85313ae54a20f3788e6f22ea8f
yMaatheus <stvgamer74@gmail.com>
2022-05-30 18:36:48 -0300

    feat: inserindo informações do filme procurando nemo à tabela BoxOffice

--- 03-back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercise.sql
+++ 03-back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercise.sql
@@ -2,3 +2,6 @@
 INSERT INTO Pixar.Movies (title, director, year, length_minutes)
 VALUES ('Monstros SA', 'Pete Docter', 2001, 92), ('Procurando Nemo', 'John Lasseter', 2003, 107),
 ('Os Incríveis', 'Brad Bird', 2004, 116), ('WALL-E', 'Pete Docter', 2008, 104);
+
+-- inserindo na tabela BoxOffice que o filme Procurando Nemo foi classificado em 6.8, fez 450 milhões no mercado interno e 370 milhões no mercado internacional:
+INSERT INTO Pixar.BoxOffice (movie_id, rating, domestic_sales, international_sales) VALUES (9, 6.8, 450000000, 370000000);
