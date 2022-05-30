b30cd282b22d845b65f286da9564e393e3682f0d
yMaatheus <stvgamer74@gmail.com>
2022-05-30 13:44:22 -0300

    feat: somando 10 e 15

--- 03-back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/exercise.cmd
+++ 03-back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/exercise.cmd
@@ -5,3 +5,7 @@ SELECT 'This is SQL Exercise, Practice and Solution';
 ## Escrevendo QUERY para exibir três números em três colunas:
 
 SELECT 1, 2, 3;
+
+## Escrevendo QUERY que soma os numeros 10 e 15:
+
+SELECT 10 + 15;
