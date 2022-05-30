5b1fd4098d6cdbc3e2c49731cb09d3ca0593aded
yMaatheus <stvgamer74@gmail.com>
2022-05-30 17:44:37 -0300

    feat: exibindo todos os fornecimentos que contenham a peça de codigo 2 em ordem alfabética de fornecedor

--- 03-back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise.sql
+++ 03-back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercise.sql
@@ -1,2 +1,6 @@
--- Exibindo todas as peças que começam com as letras GR.
+-- Exibindo todas as peças que começam com as letras GR:
 SELECT * FROM PecasFornecedores.Pecas WHERE name LIKE 'GR%';
+
+-- escreva uma query para mostrar todos os fornecimentos que contenham a peça com code 2 em ordem alfabética de fornecedor:
+
+SELECT * FROM PecasFornecedores.Fornecimentos WHERE code = 2 ORDER BY Fornecedor;
