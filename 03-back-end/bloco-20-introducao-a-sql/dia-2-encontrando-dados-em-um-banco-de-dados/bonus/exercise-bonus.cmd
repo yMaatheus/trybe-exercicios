ca31ca7162e18929b626490ee1676a0fd58f3065
yMaatheus <stvgamer74@gmail.com>
2022-05-30 17:17:03 -0300

    feat(bonus): exibindo todas as informações das peças que são providas pela empresa HAL, ordenadas pelo preço em ordem decrescente

--- 03-back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/bonus/exercise-bonus.cmd
+++ 03-back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/bonus/exercise-bonus.cmd
@@ -9,3 +9,7 @@ SELECT * FROM PiecesProviders.Provides ORDER BY Price DESC LIMIT 5;
 ## Escreva uma query para exibir o nome das empresas e preço das peças com os quatro maiores preços, começando a lista a partir do 3º item.
 
 SELECT Provider, Price FROM PiecesProviders.Provides ORDER BY Price DESC LIMIT 4 OFFSET 2;
+
+## Escreva uma query para exibir todas as informações das peças que são providas pela empresa HAL, ordenadas pelo preço em ordem decrescente:
+
+SELECT * FROM PiecesProviders.Provides WHERE Provider = 'HAL' ORDER BY Price DESC;
