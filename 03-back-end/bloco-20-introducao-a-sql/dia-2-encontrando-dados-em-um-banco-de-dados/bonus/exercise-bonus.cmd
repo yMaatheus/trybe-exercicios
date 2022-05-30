3f58e429043c8dd6d7154e35b62be764dfb28d93
yMaatheus <stvgamer74@gmail.com>
2022-05-30 17:19:37 -0300

    feat(bonus): exibindo por quantas empresas a peça 1 é provida

--- 03-back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/bonus/exercise-bonus.cmd
+++ 03-back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/bonus/exercise-bonus.cmd
@@ -6,10 +6,14 @@ SELECT Piece, Price FROM PiecesProviders.Provides WHERE Provider = 'RBT';
 
 SELECT * FROM PiecesProviders.Provides ORDER BY Price DESC LIMIT 5;
 
-## Escreva uma query para exibir o nome das empresas e preço das peças com os quatro maiores preços, começando a lista a partir do 3º item.
+## Escreva uma query para exibir o nome das empresas e preço das peças com os quatro maiores preços, começando a lista a partir do 3º item:
 
 SELECT Provider, Price FROM PiecesProviders.Provides ORDER BY Price DESC LIMIT 4 OFFSET 2;
 
 ## Escreva uma query para exibir todas as informações das peças que são providas pela empresa HAL, ordenadas pelo preço em ordem decrescente:
 
 SELECT * FROM PiecesProviders.Provides WHERE Provider = 'HAL' ORDER BY Price DESC;
+
+## Escreva uma query para exibir por quantas empresas a peça 1 é provida.
+
+SELECT COUNT(*) FROM PiecesProviders.Provides WHERE Piece = 1;
