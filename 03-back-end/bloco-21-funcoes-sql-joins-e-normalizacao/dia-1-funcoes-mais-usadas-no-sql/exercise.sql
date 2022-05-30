6449824abaed7055e6d88871eeb7fc623793c7b8
yMaatheus <stvgamer74@gmail.com>
2022-05-30 20:41:41 -0300

    feat: exibindo a diferença entre o maior e o menor salário

--- 03-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercise.sql
+++ 03-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercise.sql
@@ -1,2 +1,5 @@
 -- exibindo o maior salário da tabela:
 SELECT MAX(SALARY) FROM hr.employees;
+
+-- exibindo a diferença entre o maior e o menor salário:
+SELECT (MAX(SALARY) - MIN(SALARY)) FROM hr.employees;
