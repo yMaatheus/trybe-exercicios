1232c9b4eaaa1e3e54407180140495da5552430a
yMaatheus <stvgamer74@gmail.com>
2022-06-08 01:57:17 -0300

    feat(E03): buscando os filmes e sua avaliação em ordem decrescente.

--- 03-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/exercise.sql
+++ 03-back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/exercise.sql
@@ -8,7 +8,6 @@ FROM
     INNER JOIN pixar.box_office AS box ON movies.id = box.movie_id;
 
 -- obtendo filmes com o numero de vendas internacionais maior que nacionais.
-
 SELECT
     movies.title,
     box.domestic_sales,
@@ -17,3 +16,13 @@ FROM
     pixar.movies AS movies
     INNER JOIN pixar.box_office AS box ON movies.id = box.movie_id
     AND box.international_sales > box.domestic_sales;
+
+-- buscando os filmes e sua avaliação em ordem decrescente.
+SELECT
+    movies.title,
+    box.rating
+FROM
+    pixar.movies AS movies
+    INNER JOIN pixar.box_office AS box ON movies.id = box.movie_id
+ORDER BY
+    box.rating DESC;
