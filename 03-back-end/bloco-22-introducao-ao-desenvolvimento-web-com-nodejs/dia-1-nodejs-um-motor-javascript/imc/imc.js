70e8b3ed66b0631178e1467e989a587eea8511bd
yMaatheus <stvgamer74@gmail.com>
2022-06-16 05:09:06 -0300

    feat: informando a qual categoria o imc informado se encaixa

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/imc/imc.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/imc/imc.js
@@ -3,6 +3,19 @@ const readlineSync = require('readline-sync');
 const peso = readlineSync.questionInt('Qual o seu peso? ');
 const altura = readlineSync.questionFloat('Qual sua altura? ');
 
-const imc = peso / (altura ** 2)
+const imc = peso / (altura ** 2);
+let categoria = 'Abaixo do peso';
 
-console.log(imc);
+if (imc > 18.5 && imc < 24.9) {
+    categoria = 'Peso normal';
+} else if (imc > 25 && imc < 29.9) {
+    categoria = 'Acima do peso'
+} else if (imc > 30 && imc < 34.9) {
+    categoria = 'Obesidade grau I';
+} else if (imc > 35 && imc < 39.9) {
+    categoria = 'Obesidade grau II';
+} else if (imc > 40) {
+    categoria = 'Obesidade grau III e IV';
+}
+
+console.log(`${imc} - ${categoria}`);
