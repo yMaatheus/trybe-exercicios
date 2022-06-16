17fec2a79f01d54a2205bb6aeb54fc07d6bdd865
yMaatheus <stvgamer74@gmail.com>
2022-06-16 05:34:02 -0300

    feat: script que sorteia e numero e compara se com numero escolhido

---
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/sorteio.js
@@ -0,0 +1,13 @@
+const readlineSync = require('readline-sync');
+
+const numeroEscolhido = readlineSync.questionInt('Escolha um número entre 0 e 10: ');
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const sorteado = getRandomInt(0, 11);
+
+console.log(numeroEscolhido === sorteado ? 'Parabéns, número correto!' : `Opa, não foi dessa vez. O número era ${sorteado}`);
