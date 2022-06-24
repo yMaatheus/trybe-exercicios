57acc6681b06dc125636944dc2bf8ac9137666fd
yMaatheus <stvgamer74@gmail.com>
2022-06-24 08:17:48 -0300

    feat(E4): criando função que busca simpson por Id

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/simpsons.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/simpsons.js
@@ -1,13 +1,34 @@
 const fs = require('fs/promises');
 
+const readFile = async (file) => {
+  const data = await fs.readFile(file, { encoding: 'utf8' });
+  const array = JSON.parse(data);
+  return array;
+}
+
 const readSimpsons = async () => {
   try {
-    const data = await fs.readFile('simpsons.json', { encoding: 'utf8' });
-    const array = JSON.parse(data);
-    array.forEach(({ id, name}) => console.log(`${id} - ${name}`));
-  } catch (err) {
-    console.log(err);
+    const array = await readFile('./simpsons.json');
+    array.forEach(({ id, name }) => console.log(`${id} - ${name}`));
+  } catch (error) {
+    console.log(error);
   }
 }
 
+const getSimpsonById = async (id) => {
+  const promise = new Promise(async (resolve, reject) => {
+    const array = await readFile('./simpsons.json');
+    const simpson = array.find(({ id: simpsonId }) => +simpsonId === id);
+    if (!simpson) {
+      return reject(new Error('id não encontrado'));
+    }
+    return resolve(simpson)
+  });
+  return promise;
+}
+
 readSimpsons();
+
+getSimpsonById(1).then((simpson) => console.log(simpson)).catch((err) => console.log(err.message));
+
+getSimpsonById(11).then((simpson) => console.log(simpson)).catch((err) => console.log(err.message));
