b9ed7a7cd45d95affaa8d60dbca1fa3ba12c5408
yMaatheus <stvgamer74@gmail.com>
2022-06-26 16:56:55 -0300

    feat: criando rota PUT /users/:name/:age a qual retorna um json informando o nome e idade

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -11,4 +11,9 @@ app.post('/greetings', (req, res) => {
     return +age > 17 ? res.status(200).json({ "message": `Hello, ${name}!` } ) : res.status(401).json({ "message": "Unauthorized" });
 });
 
+app.put('/users/:name/:age', (req, res) => {
+    const { name, age } = req.params;
+    res.status(200).json({ "message": `Seu nome é ${name} e você tem ${age} anos de idade` });
+});
+
 app.listen(3001, () => console.log('listening on port 3001'));
