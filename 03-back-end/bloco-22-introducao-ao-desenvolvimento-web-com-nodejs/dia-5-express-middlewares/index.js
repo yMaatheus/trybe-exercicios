3956ccd138e68a816114fc8386db8d90d5c6dac1
yMaatheus <stvgamer74@gmail.com>
2022-06-26 18:05:42 -0300

    feat: criando rota signup a qual valida as informações cadastro e gera um token aleatorio de 16 caracteres

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/index.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 
-const salesMiddleware = require('./ middlewares/salesMiddleware');
+const validateSale = require('./middlewares/salesMiddleware');
+const validateSignup = require('./ middlewares/signupMiddleware');
+const generateToken = require('./helpers/token');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.post('/sales', salesMiddleware, async (_req, res) =>
+app.post('/sales', validateSale, async (_req, res) =>
     res.status(201).json({ "message": "Venda cadastrada com sucesso" }));
+
+app.post('/signup', validateSignup, async (_req, res) => {
+    try {
+        const token = generateToken();
+        await writeFile('token.json', JSON.stringify(token));
+        res.status(200).json({ token })
+    } catch (error) {
+        res.status(500).json({ "error": error.message });
+    }
+});
 
 app.listen(3001, () => console.log('listening on port 3001'));
