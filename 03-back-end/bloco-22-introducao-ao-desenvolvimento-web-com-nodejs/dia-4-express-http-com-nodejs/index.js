b8eeb680c6511f37b21f69c159043d5345c4262d
yMaatheus <stvgamer74@gmail.com>
2022-06-26 16:51:54 -0300

    feat(greetings): retornando hello caso o usuario enviado tenha mais de 17 anos, caso não retornar não autorizado

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -6,4 +6,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.post('/greetings', (req, res) => {
+    const { name, age } = req.body;
+    return +age > 17 ? res.status(200).json({ "message": `Hello, ${name}!` } ) : res.status(401).json({ "message": "Unauthorized" });
+});
+
 app.listen(3001, () => console.log('listening on port 3001'));
