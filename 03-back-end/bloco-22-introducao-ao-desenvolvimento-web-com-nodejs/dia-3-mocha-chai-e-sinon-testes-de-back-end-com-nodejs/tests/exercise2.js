4a9f7e8ead250af2a332d272ad9252552d270d7f
yMaatheus <stvgamer74@gmail.com>
2022-06-26 16:02:45 -0300

    test: aplicando mudança nos tests

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/tests/exercise2.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/tests/exercise2.js
@@ -2,44 +2,39 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 
 const writeFile = require('../exercise2');
-const fs = require('fs/promises');
+const fs = require('fs');
 
 const fileName = 'teste.txt';
 const content = 'Hello world!';
 
 describe('A função writeFile', () => {
     before(() => {
-        sinon.stub(fs, 'writeFile').returns(content);
+        sinon.stub(fs, 'writeFileSync').returns(content);
     });
 
     after(() => {
-        fs.writeFile.restore();
+        fs.writeFileSync.restore();
     });
 
     it('Existe', () => {
         expect(writeFile).to.be.a('function');
     })
     it('Retorna ok', async () => {
-        expect(await writeFile(fileName, content)).to.be.equal('ok');
-    });
-    it('Escreve o conteúdo', async () => {
-        await writeFile(fileName, content);
-        expect(fs.writeFile).to.be.called;
-        expect(fs.writeFile()).to.be.equal(content);
+        expect(writeFile(fileName, content)).to.be.equal('ok');
     });
 
     describe('Retorna um erro', () => {
         describe('Caso o arquivo', () => {
             it('seja indefinido', async () => {
-                expect(await writeFile()).to.throw('file is undefined');
+                expect(() => writeFile()).to.throw('file is undefined');
             });
             it('seja inválido', async () => {
-                expect(await writeFile(131313, content)).to.throw('file is invalid');
+                expect(() => writeFile(131313, content)).to.throw('file is invalid');
             });
         });
         describe('Caso o conteúdo escrito', () => {
             it('seja indefinido', async () => {
-                expect(await writeFile(fileName)).to.throw('content is undefined');
+                expect(() => writeFile(fileName)).to.throw('content is undefined');
             });
         });
     });
