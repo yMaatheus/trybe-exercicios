0f500e59811dc3ac3ba85089d29e7836d2cf76f8
yMaatheus <stvgamer74@gmail.com>
2022-06-26 17:04:03 -0300

    feat: criando rota GET que retorna json com personagens dos simpsons

--- 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ 03-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 
+const readFile = require('./readFile');
+const writeFile = require('./writeFile');
+
 const app = express();
 
 app.use(express.json());
@@ -14,6 +17,15 @@ app.post('/greetings', (req, res) => {
 app.put('/users/:name/:age', (req, res) => {
     const { name, age } = req.params;
     res.status(200).json({ "message": `Seu nome é ${name} e você tem ${age} anos de idade` });
+});
+
+app.get('/simpsons', async (req, res) => {
+    try {
+        const simpsons = await readFile('./simpsons.json');
+        res.status(200).json(simpsons);
+    } catch (error) {
+        res.status(500).json({ "error": error.message });
+    }
 });
 
 app.listen(3001, () => console.log('listening on port 3001'));
